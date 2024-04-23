6086db049a422dd2a6a45c47adfc1ce74613b77c
omkartoradaml <162466734+omkartoradaml@users.noreply.github.com>
2024-04-24 00:50:16 +0530

    uploaded script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
